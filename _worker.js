3dd0c1a0ef342624a2279cad7c99c4831234c34e
xiaomeng991117 <126213268+xiaomeng991117@users.noreply.github.com>
2025-08-06 22:18:59 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -30,7 +30,7 @@ let addressesnotlsapi = [];
 let addressescsv = [];
 let DLS = 8;
 let remarkIndex = 1;//CSV备注所在列偏移量
-let FileName = atob('5bGx5bGx6ICM5bed');
+let FileName = atob('2025');
 let BotToken;
 let ChatID;
 let proxyhosts = [];
